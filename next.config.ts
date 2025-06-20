a8d067550aa2f9a74f0891ea2c8bfcb110b958d4
miles-q-ryder <falconfire1405@gmail.com>
2025-06-20 09:09:30 -0500

    Update file next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
   output: 'export'
 };
 
