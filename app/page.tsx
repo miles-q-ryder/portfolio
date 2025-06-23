b7a126a99fbf2545c4413cebbc200d820ae116c6
miles-q-ryder <falconfire1405@gmail.com>
2025-06-23 14:12:33 -0500

    fix: remove font

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import Sections from './ui/sections';
-import { Parisienne } from 'next/font/google';
-
-const parisienne = Parisienne({
-	weight: '400'
-});
 
 export default function Home() {
 	return (
 		<div className='items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
 			<main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
-				<h1 className={`self-center text-4xl text-teal-950 ${parisienne}`}>
+				<h1 className={`self-center text-4xl text-teal-950`}>
 					Miles Q. Ryder
 				</h1>
 				{/* Write something personal */}
