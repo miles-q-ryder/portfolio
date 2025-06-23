46e5d1f1c845847f61f05f317385ac3ad6c7f72b
miles-q-ryder <falconfire1405@gmail.com>
2025-06-23 14:14:09 -0500

    fix?

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<div className='items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
 			<main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
-				<h1 className={`self-center text-4xl text-teal-950`}>
+				<h1 className='self-center text-4xl text-teal-950'>
 					Miles Q. Ryder
 				</h1>
 				{/* Write something personal */}
